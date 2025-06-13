a06f9946da29d7092c8b892ff1f3c54265b37e39
ririkuchan <ririkuchan@gmail.com>
2025-06-13 22:39:29 +0900

    server.js fixed

--- server.js
+++ server.js
@@ -36,6 +36,9 @@ app.use(
         secret: process.env.SESSION_SECRET,
         resave: false,
         saveUninitialized: false,
+        cookie: {
+            secure: process.env.NODE_ENV === 'production', // 本番(=Render)だけ secure:true にする
+        },
     })
 );
 
