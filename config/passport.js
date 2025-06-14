f5dbc2dd991149287ae039a6590b198f69649fa2
ririkuchan <ririkuchan@gmail.com>
2025-06-14 10:10:14 +0900

    passport.js fixed

--- config/passport.js
+++ config/passport.js
@@ -1,26 +1,20 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-passport.use(
-    new GoogleStrategy(
-        {
-            clientID: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: '/auth/google/callback',
-        },
-        (accessToken, refreshToken, profile, done) => {
-            console.log('Google profile:', profile);
-            // 今はユーザーDBに保存せずそのまま返す
-            done(null, profile);
-        }
-    )
-);
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/auth/google/callback'   // ← ここ
+},
+    function (accessToken, refreshToken, profile, done) {
+        return done(null, profile);
+    }
+));
 
-// セッションの serialize/deserialize
 passport.serializeUser((user, done) => {
     done(null, user);
 });
 
-passport.deserializeUser((obj, done) => {
-    done(null, obj);
+passport.deserializeUser((user, done) => {
+    done(null, user);
 });
